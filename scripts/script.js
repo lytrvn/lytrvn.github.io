93db51eb023393e1c0aec14ac5fc8b585bdeed48
Lyudmila Travkina <lytrvn@gmail.com>
2026-03-12 15:41:58 +0300

    url

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,14 @@ const path = window.location.pathname;
 
 const id = path.replace('/', '');
 console.log(id);
+
+fetch("/lytrvn.github.io/guests.json")
+.then(r => r.json())
+.then(data => {
+    const guest = data[id];
+    if(!guest) {
+        document.querySelector('.guest-name').textContent = 'Дорогой гость!';
+        return;
+    }
+    document.querySelector('.guest-name').textContent = guest.name;
+})
