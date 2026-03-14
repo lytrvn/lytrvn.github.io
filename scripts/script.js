e278ff9d1984b462c834130bcd2dab785c337d38
Lyudmila Travkina <lytrvn@gmail.com>
2026-03-15 00:29:34 +0300

    final

--- scripts/script.js
+++ scripts/script.js
@@ -17,6 +17,7 @@ fetch("/guests.json")
     }
     document.querySelector('.guest-greeting').textContent = greeting;
     document.querySelector('.guest-name').textContent = fullName;
+    document.getElementById('message').value = `Гость ${fullName} подтвердил свое присутствие!`;
 })
 
 // counter
@@ -30,7 +31,7 @@ const numbers = {
 
 const weddingDate = new Date('2026-04-24T13:00:00');
 
-function countdown() {
+function countdown(words) {
     const currentDate = new Date();
     const difference = weddingDate - currentDate;
     if(difference <= 0) {
@@ -51,6 +52,7 @@ function countdown() {
     numbers.minutes.textContent = String(minutes).padStart(2, '0');
     numbers.seconds.textContent = String(seconds).padStart(2, '0');
 }
+
 
 countdown();
 const timeInterval = window.setInterval(countdown, 1000);
